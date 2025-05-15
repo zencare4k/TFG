6f71ab21815e797713f41bae50cc9a25c50e6386
zencare4k <cmarrom@adaits.es>
2025-05-15 21:44:23 +0200

    xd

--- Frontend/src/services/carrousel_API.js
+++ Frontend/src/services/carrousel_API.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/carousel"; // Cambia la URL según tu backend
+const API_URL = "http://localhost:5000/api/carousel"; 
 
-// Obtiene las imágenes del carrusel desde la base de datos
-export const fetchCarouselImages = async () => {
+ export const fetchCarouselImages = async () => {
     try {
         const response = await axios.get(`${API_URL}/images`);
-        return response.data; // Asegúrate de que el backend devuelva las imágenes en el formato esperado
+        return response.data;  
     } catch (error) {
         console.error("Error al obtener las imágenes del carrusel:", error);
         throw error;
