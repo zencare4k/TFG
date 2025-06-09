6a9000710d7f7a108bdca0ddf02a24eeb82e4882
zencare4k <cmarrom@adaits.es>
2025-06-10 00:47:52 +0200

    asfasdfasdf

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,6 @@
 import app from './index.js';
-import cors from 'cors';
-import mainRouter from './src/Routes/index.js';
 
 const PORT = process.env.PORT || 5000;
-
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://tfg-hnwj.vercel.app",
-  "https://tfg-61pu.vercel.app"
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
-
-app.use('/api', mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
