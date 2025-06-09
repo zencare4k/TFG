4c262d833e54bc62fa6ac8c71e65400b4bcc6372
zencare4k <cmarrom@adaits.es>
2025-06-10 00:16:09 +0200

    asdfasdfasdf

--- Backend/server.js
+++ Backend/server.js
@@ -10,18 +10,6 @@ app.use(cors({
   credentials: true
 }));
 
-// Añade manualmente las cabeceras CORS para Render
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 app.use('/api', mainRouter);
 
 app.listen(PORT, () => {
