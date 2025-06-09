29f03edbac9b350cd8842a439cd6da988fa28b27
zencare4k <cmarrom@adaits.es>
2025-06-10 00:06:37 +0200

    asdfssdfasd

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,10 @@ import cors from 'cors';
 import mainRouter from './src/Routes/index.js';
 
 const PORT = process.env.PORT || 5000;
+
+// Permite CORS para cualquier origen, incluso con credenciales
 app.use(cors({
-  origin: (origin, callback) => callback(null, origin),
+  origin: (origin, callback) => callback(null, true),
   credentials: true
 }));
 
